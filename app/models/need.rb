a08e09a8ea9df7fb025dc91816d6d7162fa1047e
Stuart Gale <bishboria@gmail.com>
2013-10-02 10:25:06 +0000

    Convert Need to ActiveModel instance.

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,4 +1,15 @@
 require "active_model"
 
 class Need
+  extend ActiveModel::Naming
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+
+  attr_accessor :role
+
+  validates_presence_of :role
+
+  def persisted?
+    false
+  end
 end
