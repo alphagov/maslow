83b4a0542a9ecf77c2c95251a02ee8eac10af355
Jordan Hatch <jordan@jordanh.net>
2013-11-08 12:22:46 +0000

    Add method to stub api responses
    Allow a need to be stubbed based on a JSON file in
    the test/fixtures/needs directory.

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -1,8 +1,11 @@
 require_relative 'test_helper'
+
 require 'capybara/rails'
+require 'gds_api/test_helpers/need_api'
 
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
+  include GdsApi::TestHelpers::NeedApi
 
   def login_as(user)
     GDS::SSO.test_user = user
@@ -11,5 +14,15 @@ class ActionDispatch::IntegrationTest
 
   def click_on_first(selector)
     first(:button, selector).click
+  end
+
+  def setup_need_api_responses(need_id)
+    json = File.read Rails.root.join("test", "fixtures", "needs", "#{need_id}.json")
+
+    entire_need = JSON.parse(json)
+    need_api_has_need(entire_need)
+
+    basic_need = entire_need.slice("id", "role", "goal", "benefit", "organisation_ids", "organisations")
+    need_api_has_needs([basic_need])
   end
 end
