b7b7adbbf3e1cb6fe0ce81b51e65c6575ce5e4da
Jordan Hatch <jordan@jordanh.net>
2013-11-22 14:17:17 +0000

    Return a 422 when the CSRF token is invalid
    At the moment, when the CSRF token is invalid on
    a HTTP POST, we acknowledge it by warning in the
    logs, but we continue the request. This is a Rails 
    default.
    
    Instead, we should explicitly reject the request 
    to the user. Rails encourages us to override the
    verify_authenticity_token method in the controller
    in order to add our own behaviour.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  rescue_from ActionController::InvalidAuthenticityToken do
+    render "public/422", layout: false, status: 422
+  end
+
   include GDS::SSO::ControllerMethods
 
   before_filter :authenticate_user!
   before_filter :require_signin_permission!
+
+  private
+    def verify_authenticity_token
+      raise ActionController::InvalidAuthenticityToken unless verified_request?
+    end
 end
