e9ff7733a884c56d167e5e99ea9bc67c1563fb48
Jordan Hatch <jordan@jordanh.net>
2013-11-25 15:42:05 +0000

    Initialize a Content API client on the Maslow module

--- config/initializers/gds_api.rb
+++ config/initializers/gds_api.rb
@@ -1,3 +1,5 @@
 require 'gds_api/need_api'
+require 'gds_api/content_api'
 
 Maslow.need_api = GdsApi::NeedApi.new(Plek.current.find('need-api'), API_CLIENT_CREDENTIALS)
+Maslow.content_api = GdsApi::ContentApi.new(Plek.current.find('contentapi'), API_CLIENT_CREDENTIALS)
