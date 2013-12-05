1af362442fc6ca369ad83ebdd1a3a1c2fdfee1fb
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-12-05 11:45:44 +0000

    Use something Railsier to set feedback address.

--- test/integration/feedback_link_test.rb
+++ test/integration/feedback_link_test.rb
@@ -9,13 +9,13 @@ class FeedbackLinkTest < ActionDispatch::IntegrationTest
 
   context "on the need list" do
     should "show a feedback link if the address is set" do
-      Maslow.stubs(:feedback_address).returns("foo@example.com")
+      Maslow::Application.config.stubs(:feedback_address).returns("foo@example.com")
       visit "/needs"
       assert page.has_link?("Send feedback", href: "mailto:foo@example.com")
     end
 
     should "not show a feedback link if the address is not set" do
-      Maslow.stubs(:feedback_address).returns(nil)
+      Maslow::Application.config.stubs(:feedback_address).returns(nil)
       visit "/needs"
       refute page.has_link?("Send feedback")
     end
