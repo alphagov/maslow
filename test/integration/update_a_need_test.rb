40c658f04ca2007bb3d443b3773614a9afa13965
Stuart Gale <bishboria@gmail.com>
2013-12-16 12:23:05 +0000

    Code and integration test for closing a need as a duplicate

--- test/integration/update_a_need_test.rb
+++ test/integration/update_a_need_test.rb
@@ -295,4 +295,53 @@ class UpdateANeedTest < ActionDispatch::IntegrationTest
       assert page.has_content?("We had a problem marking the need as out of scope")
     end
   end
+
+  context "closing a need as a duplicate" do
+    setup do
+      @need = need_hash
+      @duplicate = need_hash.merge(
+        "duplicate_of" => nil,
+        "id" => "100002"
+      )
+      need_api_has_needs([@need,@duplicate]) # For need list
+      content_api_has_artefacts_for_need_id("100002", [])
+
+      @api_url = Plek.current.find('need-api') + '/needs/100002'
+    end
+
+    should "be able to close a need as a duplicate" do
+      need_api_has_need(@duplicate) # For individual need
+      request_body = {
+        "duplicate_of" => "100001",
+        "author" => {
+          "name" => stub_user.name,
+          "email" => stub_user.email,
+          "uid" => stub_user.uid
+        }
+      }
+
+      request = stub_request(:put, @api_url+'/closed').with(:body => request_body.to_json)
+
+      visit "/needs"
+      click_on "100002"
+      click_on "Edit"
+      fill_in("Duplicate of", with: 100001)
+
+      get_request = stub_request(:get, @api_url).to_return(
+        :body =>
+          { "_response_info" => { "status" => "ok" },
+            "id" => "100002",
+            "role" => "User",
+            "goal" => "find my local register office",
+            "benefit" => "I can find records of birth, marriage or death",
+            "duplicate_of" => "100001"
+          }.to_json
+      )
+
+      click_on_first_button("Close as duplicate")
+
+      assert page.has_content?("Need closed as a duplicate of 100001")
+      assert page.has_no_button?("Edit")
+    end
+  end
 end
