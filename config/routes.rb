30bb104c4dfcf575a74ff34b2ea103db99f77d31
Stuart Gale <bishboria@gmail.com>
2014-01-13 10:10:50 +0000

    Remove code from Export needs task...

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Maslow::Application.routes.draw do
 
   get "/healthcheck" => Proc.new { [200, {"Content-type" => "text/plain"}, ["OK"]] }
 
-  get "needs/export", to: "needs#export"
   resources :needs, except: [:destroy], constraints: { id: /[0-9]+/ } do
     member do
       get :revisions
