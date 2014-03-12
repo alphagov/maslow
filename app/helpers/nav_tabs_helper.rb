7f4197a3348d850ae587bcc68dab02d9d7a8d338
Jake Benilov <benilov@gmail.com>
2014-03-12 14:20:36 +0000

    Small cleanup of NavTabsHelper

--- app/helpers/nav_tabs_helper.rb
+++ app/helpers/nav_tabs_helper.rb
@@ -2,11 +2,10 @@ module NavTabsHelper
   def nav_tabs_for(need)
     tabs = {
       "View" => need_path(need),
-      "Edit" => edit_need_path(need),
       "Actions" => actions_need_path(need),
       "History & Notes" => revisions_need_path(need)
     }
-    tabs.delete("Edit") if need.duplicate_of.present?
+    tabs["Edit"] = edit_need_path(need) unless need.duplicate?
     tabs
   end
 end
