234f04277ebe98391dd7bdece8a2538c14ddb88a
Jake Benilov <benilov@gmail.com>
2014-12-07 20:49:46 +0000

    Update need status form JS to work with initial values

--- app/assets/javascripts/modules/decode_on_need.js
+++ app/assets/javascripts/modules/decode_on_need.js
@@ -7,24 +7,17 @@
   Modules.DecideOnNeed = function() {
     var that = this;
     that.start = function(element) {
-      var $allFieldsets = element.find('fieldset[data-status-description]');
+      element.find('input.new-status-description').change(displayRelevantFormFieldset);
+      displayRelevantFormFieldset(); // this arranges the form up correctly when it's first loaded
 
-      element.find('input.new-status-description').
-        change(enableSubmitButton).
-        change(displayRelevantFormField);
-
-      $allFieldsets.hide();
-
-      function displayRelevantFormField() {
-        var selectedStatusDescription = $(this).val();
+      function displayRelevantFormFieldset() {
+        var selectedStatusDescription = element.find('input.new-status-description:checked').val();
         var $relevantFieldset = element.find('fieldset[data-status-description="' + selectedStatusDescription + '"]');
 
         $relevantFieldset.show();
-        $allFieldsets.not($relevantFieldset).hide();
-      };
 
-      function enableSubmitButton() {
-        element.find('.decide-on-need-submit-button').attr('disabled', false);
+        var $allFieldsets = element.find('fieldset[data-status-description]');
+        $allFieldsets.not($relevantFieldset).hide();
       };
     };
   };
