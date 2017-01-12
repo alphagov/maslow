1b5926ab7a23f622d974918bad50a8f1479c2ba5
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-01-12 11:20:40 +0000

    WIP factories

--- test/factories/factories.rb
+++ test/factories/factories.rb
@@ -11,4 +11,103 @@ FactoryGirl.define do
       permissions { %w(signin editor admin) }
     end
   end
+
+  sequence :content_id do |_|
+    SecureRandom.uuid
+  end
+
+  factory :need_content_item, class: Hash do
+    content_id
+    base_path "/needs/slug"
+    title "Example need"
+    description "This is the summary of example need"
+    schema_name "need"
+    publishing_app "maslow"
+    rendering_app "info-frontend"
+    locale "en"
+    phase "live"
+    redirects []
+    update_type "major"
+    public_updated_at "2015-11-16T11:53:30+00:00"
+    first_published_at nil
+    last_edited_at "2015-11-15T11:53:30"
+    publication_state "draft"
+    state_history {
+      { "1": "draft" }
+    }
+
+    routes {
+      [
+        {
+          "path" => base_path,
+          "type" => "exact",
+        }
+      ]
+    }
+
+    details { default_details }
+
+    transient do
+      default_details {
+        {
+          "applies_to_all_organisations": false,
+          "benefit": "claim my entitlement",
+          "duplicate_of": 100003,
+          "goal": "find out if an estate is claimable and how to make a claim on an estate",
+          "legislation": nil,
+          "met_when": [
+                       "Knows how to find the list and claim an estate",
+                       "Claims an estate"
+                     ],
+          "need_id": 100002,
+          "organisation_ids": [
+            "bona-vacantia",
+            "treasury-solicitor-s-department"
+          ],
+          "other_evidence": "Relatives are entitled to claim up to 30 years after the death; it is their money to claim and it can only be claimed through the Treasury Solicitor",
+          "out_of_scope_reason": nil,
+          "role": "relative of a deceased person"
+        }
+      }
+      default_metadata { {} }
+    end
+
+    initialize_with {
+      merged_details = default_details.deep_stringify_keys.deep_merge(details.deep_stringify_keys)
+      attributes.merge(details: merged_details)
+    }
+
+    # This is the default document state.
+    trait :draft do
+    end
+
+    trait :published do
+      publication_state 'published'
+      first_published_at "2015-11-15T00:00:00+00:00"
+      state_history {
+        { "1": "published" }
+      }
+    end
+
+    trait :redrafted do
+      state_history {
+        { "2": "draft", "1": "published" }
+      }
+
+      publication_state 'draft'
+      first_published_at "2015-11-15T00:00:00+00:00"
+
+      update_type "major"
+    end
+
+    trait :unpublished do
+      publication_state 'unpublished'
+      first_published_at "2015-11-15T00:00:00+00:00"
+      state_history {
+        { "1": "unpublished" }
+      }
+    end
+
+    to_create(&:deep_stringify_keys!)
+  end
 end
