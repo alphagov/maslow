1ebbaea53e1623b1ed07377a9a79126472572925
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-01-18 11:44:07 +0000

    Update BookmarksControllerTest
    Replace uses of need ids with content ids.

--- test/functional/bookmarks_controller_test.rb
+++ test/functional/bookmarks_controller_test.rb
@@ -3,43 +3,23 @@ require_relative '../integration_test_helper'
 class BookmarksControllerTest < ActionController::TestCase
   include GdsApi::TestHelpers::Organisations
 
-  def need
-    {
-      "id" => "10001",
-      "role" => "parent",
-      "goal" => "apply for a primary school place",
-      "benefit" => "my child can start school",
-      "organisation_ids" => ["department-for-education"],
-      "organisations" => [
-        {
-          "id" => "department-for-education",
-          "name" => "Department for Education",
-        }
-      ],
-      "applies_to_all_organisations" => false,
-      "justifications" => [
-        "it's something only government does",
-        "the government is legally obliged to provide it"
-      ],
-      "impact" => "Has serious consequences for the day-to-day lives of your users",
-      "met_when" => [
-        "The user applies for a school place"
-      ],
-      "status" => {
-        "description" => "proposed"
-      }
-    }
-  end
+  need_content_item = FactoryGirl.create(:need_content_item)
 
   setup do
     login_as_stub_user
-    need_api_has_need_ids([need])
+    publishing_api_has_item(need_content_item)
+    publishing_api_has_expanded_links(
+      content_id: need_content_item["content_id"],
+      expanded_links: {
+        organisations: []
+      }
+    )
     organisations_api_has_organisations({})
   end
 
   context "GET bookmarks" do
     should "be successful" do
-      stub_user.expects(:bookmarks).returns([10001])
+      stub_user.expects(:bookmarks).returns([need_content_item["content_id"]])
 
       get :index
       assert_response :success
@@ -48,11 +28,11 @@ class BookmarksControllerTest < ActionController::TestCase
 
   context "POST toggle_bookmarks" do
     should "toggle the bookmark" do
-      stub_user.expects(:toggle_bookmark).with(10001)
+      stub_user.expects(:toggle_bookmark).with(need_content_item["content_id"])
 
       post :toggle, {
         "bookmark" => {
-          "need_id" => "10001",
+          "content_id" => need_content_item["content_id"],
           "redirect_to" => "/foo"
         }
       }
@@ -61,7 +41,7 @@ class BookmarksControllerTest < ActionController::TestCase
     should "redirect to the correct page" do
       post :toggle, {
         "bookmark" => {
-          "need_id" => "10001",
+          "content_id" => need_content_item["content_id"],
           "redirect_to" => "/needs"
         }
       }
@@ -69,7 +49,7 @@ class BookmarksControllerTest < ActionController::TestCase
 
       post :toggle, {
         "bookmark" => {
-          "need_id" => "10001",
+          "content_id" => need_content_item["content_id"],
           "redirect_to" => "/bookmarks"
         }
       }
@@ -79,21 +59,11 @@ class BookmarksControllerTest < ActionController::TestCase
     should "redirect unknown paths to /needs" do
       post :toggle, {
         "bookmark" => {
-          "need_id" => "10001",
+          "content_id" => need_content_item["content_id]"],
           "redirect_to" => "http://foo.com"
         }
       }
       assert_redirected_to "/needs"
-    end
-
-    should "should not accept an invalid need id" do
-      post :toggle, {
-        "bookmark" => {
-          "need_id" => :foo,
-          "redirect_to" => "/needs"
-        }
-      }
-      assert_equal "Cannot bookmark invalid need id", @controller.flash[:error]
     end
   end
 end
