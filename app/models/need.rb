84132c82872872e1e0e6c1fa40c947d06f32ee46
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-01-20 17:36:58 +0000

    Delegate to Need.find in Need.by_content_ids
    To reduce duplication.

--- app/models/need.rb
+++ app/models/need.rb
@@ -127,10 +127,7 @@ class Need
   # Note that this returns the entire set of matching ids and not a
   # PaginatedList
   def self.by_content_ids(*content_ids)
-    content_ids.map do |content_id|
-      response = Maslow.publishing_api_v2.get_content(content_id)
-      need_from_publishing_api_payload(response.parsed_content)
-    end
+    content_ids.map { |content_id| Need.find(content_id) }
   end
 
   # Retrieve a need from the Publishing API, or raise NotFound if it doesn't exist.
