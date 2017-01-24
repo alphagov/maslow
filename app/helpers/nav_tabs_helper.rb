5cfa84abf9074ce05a97d32d410da2f04720c19b
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-01-24 17:51:54 +0000

    Replace use of duplicate? with unpublished?

--- app/helpers/nav_tabs_helper.rb
+++ app/helpers/nav_tabs_helper.rb
@@ -2,7 +2,7 @@ module NavTabsHelper
   def nav_tabs_for(need)
     tabs = []
     tabs << ["View", need_path(need.id)]
-    tabs << ["Edit", edit_need_path(need.id)] if !need.duplicate? && current_user.can?(:update, Need)
+    tabs << ["Edit", edit_need_path(need.id)] if !need.unpublished? && current_user.can?(:update, Need)
     tabs << ["Actions", actions_need_path(need.id)] if current_user.can?(:perform_actions_on, Need)
     tabs << ["History & Notes", revisions_need_path(need.id)]
     tabs
