70c4310207087a8b71a28313c7c6d7beb60d5d99
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-02-02 13:43:47 +0000

    Fix the BookmarksControllerTest
    Change from stubbing expanded links to links to match change in the
    Need class, also add stub for linkables, as this is now how
    organisations are fetched.

--- test/functional/bookmarks_controller_test.rb
+++ test/functional/bookmarks_controller_test.rb
@@ -8,10 +8,11 @@ class BookmarksControllerTest < ActionController::TestCase
   setup do
     login_as_stub_user
     publishing_api_has_item(need_content_item)
-    publishing_api_has_expanded_links(
-      content_id: need_content_item["content_id"],
-      expanded_links: {
-        organisations: []
+    publishing_api_has_linkables([], document_type: "organisation")
+    publishing_api_has_links(
+      "content_id" => need_content_item["content_id"],
+      "links" => {
+        "organisations" => []
       }
     )
     organisations_api_has_organisations({})
