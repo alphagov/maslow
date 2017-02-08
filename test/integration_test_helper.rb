d37df8b4d6d1a5d7da498829c0e3470949d466d9
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-02-08 11:19:44 +0000

    Remove setup_need_api_repsonses test helper
    As Maslow no longer uses the Need API.

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -15,16 +15,4 @@ class ActionDispatch::IntegrationTest
   def click_on_first_button(selector)
     first(:button, selector).click
   end
-
-  def setup_need_api_responses(need_id, artefacts = [])
-    json = File.read Rails.root.join("test", "fixtures", "needs", "#{need_id}.json")
-
-    entire_need = JSON.parse(json)
-    need_api_has_need(entire_need)
-
-    basic_need = entire_need.slice("id", "role", "goal", "benefit", "organisation_ids", "organisations", "status")
-    need_api_has_needs([basic_need])
-
-    content_api_has_artefacts_for_need_id(basic_need["id"], artefacts)
-  end
 end
