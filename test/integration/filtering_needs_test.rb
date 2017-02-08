6239b74a4300ed930b562540b90e47a7ac122af7
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-02-08 10:56:38 +0000

    Remove testing for filtering by organisations
    As this functionality has been removed.

--- test/integration/filtering_needs_test.rb
+++ test/integration/filtering_needs_test.rb
@@ -55,31 +55,8 @@ class FilteringNeedsTest < ActionDispatch::IntegrationTest
       need_api_has_needs_for_search_and_filter("primary", "department-for-education", [@needs[0]])
     end
 
-    should "display needs related to an organisation" do
-      visit "/needs"
-
-      assert page.has_text?("10001")
-      assert page.has_text?("Apply for a primary school place")
-      assert page.has_text?("Department for Education")
-
-      assert page.has_text?("10003")
-      assert page.has_text?("Find out about becoming a British citizen")
-      assert page.has_text?("Home Office")
-      assert page.has_text?("HM Passport Office")
-
-      select("Department For Education [DFE]", from: "Filter needs by organisation:")
-      click_on_first_button("Filter")
-
-      within "#needs" do
-        assert page.has_text?("Department for Education")
-        assert page.has_text?("Apply for a primary school place")
-        assert page.has_no_text?("Find out about becoming a British citizen")
-      end
-    end
-
     should "display needs related to an organisation and filtered by text" do
       visit "/needs"
-      select("Department For Education [DFE]", from: "Filter needs by organisation:")
       click_on_first_button("Filter")
 
       within "#needs" do
@@ -95,31 +72,6 @@ class FilteringNeedsTest < ActionDispatch::IntegrationTest
         assert page.has_text?("Apply for a primary school place")
         refute page.has_text?("Apply for a secondary school place")
         refute page.has_text?("find out about becoming a British citizen primary")
-      end
-    end
-
-    context "filtering from showing a need" do
-      setup do
-        need_api_has_need(@needs[0])
-        content_api_has_artefacts_for_need_id("10001", [])
-      end
-
-      should "filter when clicking on an organisation while showing a need" do
-        visit "/needs"
-        click_on "10001"
-
-        within ".need-organisations" do
-          assert page.has_link?("Department for Education",
-                                href: needs_url(organisation_id: "department-for-education"))
-        end
-
-        click_on "Department for Education"
-
-        within "#needs" do
-          assert page.has_text?("Department for Education")
-          assert page.has_text?("Apply for a primary school place")
-          assert page.has_no_link?("Department for Education")
-        end
       end
     end
   end
