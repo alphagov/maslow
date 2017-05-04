22d20f4b0ed6657e65903de17d41c6fd2462ad05
Isabell Long <isabell@issyl0.co.uk>
2017-05-04 12:27:51 +0000

    The bundle exec app:update command modified some files
    - It tried to create `config/puma.rb` and `config/cable.yml`, but we
      don't use those in Production, so exclude them as per
      https://docs.publishing.service.gov.uk/manual/upgrade-rails.html#don39t-include-actioncable-or-puma-configs.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 # Pick the frameworks you want:
 # require "active_record/railtie"
@@ -7,6 +7,8 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Maslow
