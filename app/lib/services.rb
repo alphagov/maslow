1091578672cdbb1eb749c3281f0c6b3e5362f7da
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-09-06 10:24:16 +0100

    Fix naming for a memoized value

--- app/lib/services.rb
+++ app/lib/services.rb
@@ -3,7 +3,7 @@ require 'gds_api/publishing_api_v2'
 
 module Services
   def self.publishing_api_v2
-    @publishing_api ||= GdsApi::PublishingApiV2.new(
+    @publishing_api_v2 ||= GdsApi::PublishingApiV2.new(
       Plek.current.find('publishing-api'),
       bearer_token: ENV['PUBLISHING_API_BEARER_TOKEN'] || 'example'
     )
