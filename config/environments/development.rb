af3d6401fe74b2f8427a498d7954da0280644f7a
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2020-02-24 10:27:13 +0000

    Run `bundle exec rails app:update`
    I checked the new rails 6 defaults in
    `config/initializers/new_framework_defaults_6_0.r`.
    This article explains them very nicely:
    https://medium.com/@dylansreile/rails-6-0-new-framework-defaults-what-they-do-and-how-to-safely-uncomment-them-586146f371e8
    
    It seems we can use all the new defaults.
    
    Running `bundle exec rails app:update` also created another file:
    `config/initializers/content_security_policy.rb`.
    This is to define an application-wide content security policy, but since
    this is out of scope for this specific piece of work, I'll leave it
    commented (as it was created).
    
    Trello card: https://trello.com/c/z2zjkJ1R/1731-3-upgrade-maslow-to-rails-6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,12 +13,14 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=172800",
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -34,9 +36,6 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
-  # config.active_record.migration_error = :page_load
-
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -45,11 +44,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
   config.hosts << "maslow.dev.gov.uk"
 end
