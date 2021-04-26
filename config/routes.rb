8490841d2866d33658566bc2b18daa39105bd968
Michael Walker <mike@barrucadu.co.uk>
2021-04-26 13:53:11 +0100

    Remove legacy /healthcheck route
    govuk-puppet and govuk-aws have now been updated to only use the new
    routes, so the old one is no longer needed.
    
    See RFC 141 for more information.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get "/healthcheck",
-      to: GovukHealthcheck.rack_response(
-        GovukHealthcheck::Mongoid,
-      )
-
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response(
     GovukHealthcheck::Mongoid,
