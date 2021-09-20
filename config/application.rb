8e93f69c239d38a93c15b1258327a64cc6ec06f1
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2021-09-20 16:39:38 +0100

    Run rake app:update

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,12 @@
 require_relative "boot"
 
+require "rails"
 # Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
@@ -12,6 +16,7 @@ Bundler.require(*Rails.groups)
 
 module Maslow
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
